6dd735e1509005334fa7d789d65367cb3b4a6d43
Ross Murray <rossmichaelm@gmail.com>
2019-10-04 20:44:55 +0100

    complete minsSwaps

--- minSwaps2.py
+++ minSwaps2.py
@@ -1,30 +1,34 @@
 def minimumSwaps(arr):
     swaps = 0
-    minVal = min(arr)
-    maxVal = max(arr)
+    i = 0
 
-    for i in range(minVal, maxVal):
-
-        # find current number
-        iLoc = arr.index(i + 1)
+    # loop through array
+    while i < len(arr):
 
         # if number not in correct place, swap it
-        if iLoc != i:
-            arr[iLoc], arr[i] = arr[i], arr[iLoc]
+        if arr[i] == i + 1:
+            i += 1
+        else:
+            # tempNum is required because is changes midway through swap
+            tempNum = arr[i]
+            arr[i], arr[tempNum - 1] = arr[tempNum - 1], arr[i]
             swaps += 1
-            arr[i]
-            print(i+1)
 
     return swaps
 
+
 if __name__ == "__main__":
+
     f = open("minSwapVals.txt", "r")
     if f.mode == 'r':
         contents = f.read()
         numList = contents.split()
         intList = list(map(int, numList))
         res = minimumSwaps(intList)
-        print(res)
+        print(intList[:33])
+
     # inputArray = [1, 3, 5, 2, 4, 6, 7]
     # res = minimumSwaps(inputArray)
-    # print(res)
+    print(res)
+    
+
