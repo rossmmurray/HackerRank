d211892fbe02b89687d3d1a0ec49303a00a2fa9f
Ross Murray <rossmichaelm@gmail.com>
2019-10-08 18:34:30 +0100

    start array manip

---
+++ arrayManipulation.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the arrayManipulation function below.
+def arrayManipulation(n, queries):
+    initialList = [0] * n
+    for query in queries:
+        pass
+    return queries
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = [10, [[1, 2, 100], [2, 5, 100], [3, 4, 100]]]
+
+    n = int(nm[0])
+
+    m = nm[1]
+
+    result = arrayManipulation(n, m)
+
+    print(result)
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
