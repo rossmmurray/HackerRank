52d59737b06f563f93ae52b08c3b407c1678ea88
Ross Murray <rossmichaelm@gmail.com>
2019-10-10 01:15:29 +0100

    make one a special case, better but doesnt solve all use cases

--- DictHashMaps/countTriplets.py
+++ DictHashMaps/countTriplets.py
@@ -5,24 +5,37 @@ import os
 import random
 import re
 import sys
-from itertools import combinations
+from collections import Counter
 
 
 # Complete the countTriplets function below.
 def countTriplets(arr, r):
+
+    # done with binomial n choose k formula and simplified
+    if r == 1:
+        l = len(arr)
+        return l * (l - 1) * (l - 2) // 6
+    
+    # for when r != 1
+    # FYI it doesn't matter if it's sorted
     count = 0
-    for i, iVal in list(enumerate(arr))[:-2]:
-        for j, jVal in list(enumerate(arr))[i + 1:-1]:
-            if iVal * r == jVal:
-                for _k, kVal in list(enumerate(arr))[j + 1:]:
-                    if jVal * r == kVal:
-                        count += 1
+    arrCount = Counter(arr)
+    for i in arrCount:
+        # check if there is a next element which satisfied i * r, then i * r * r
+        # tempArrCount is used since the arrCount[i] stuff would create new zeros
+        tempArrCount = arrCount.copy()
+        iCount = tempArrCount[i]
+
+        jCount = tempArrCount[i * r]
+        kCount = tempArrCount[i * r * r]
+        count += iCount * jCount * kCount
+
     return count
 
 
 if __name__ == '__main__':
-    r = 5
-    arr = list(map(int, '1 5 5 25 125'.rstrip().split()))
+    r = 1
+    arr = list(map(int, '1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1'.rstrip().split()))
 
     ans = countTriplets(arr, r)
     print(ans)
